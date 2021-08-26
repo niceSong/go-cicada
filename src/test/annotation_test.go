ca65200f5c77fee639263e35fb3d5dcd4d8cddce
tangyuyao <1417532400@qq.com>
2021-08-26 10:42:35 +0800

    go-cicada to goCicada

--- src/test/annotation_test.go
+++ src/test/annotation_test.go
@@ -13,6 +13,6 @@ func TestAnnotation(t *testing.T) {
 	l := logrus.New()
 	entry := l.WithFields(logrus.Fields{"component": "UDM", "category": "HTTP"})
 	exception.AnnotationHandler("src/obj")
-	throwable := exception.Eb5gcErrorMap["AuthenticationError"](errors.New("haha"), entry, http.StatusBadRequest)
+	throwable := exception.Eb5gcErrorMap["AuthenticationError"](entry, http.StatusBadRequest, errors.New("haha"))
 	fmt.Println(throwable)
 }
